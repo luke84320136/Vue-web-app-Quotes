e1dfb1bfccefcceeba78eabb855eeba5c29e5fb3
lukelan <luke84320136@gmail.com>
2017-09-03 17:27:16 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="container">
+      <app-new-quote @addQuote="newQuote"></app-new-quote> <!-- 8/25 5.bind NewQuotes methods-->
+      <app-quote-grid :quotes="quotes"></app-quote-grid>
+      <!-- 8/25 7.Add delete button -->
+      <!-- <div class="row">
+        <div class="col-sm-12">
+
+        </div>
+      </div> -->
+    </div>
+</template>
+
+<script>
+    import QuoteGrid from './components/QuoteGrid.vue'
+    import NewQuote from './components/NewQuote.vue' // 8/25 3.import NewQuote
+
+    export default {
+        data () {
+          return {
+            quotes: [
+              'Quote Here'
+            ],
+            maxQuotes: 10
+          }
+        },
+        methods: {
+          // 8/25 6. newQuote methods，新增quote至quotes
+          newQuote (quote) {
+            this.quotes.push(quote)
+          }
+        },
+        components: {
+          appQuoteGrid: QuoteGrid,
+          appNewQuote: NewQuote // 8/25 4. add components
+        }
+    }
+</script>
+
+<style>
+</style>
---
+++ src/components/NewQuote.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="row">
+    <form>
+      <div class="col-sm-8 col-sm-offset-2 col-xs-12 col-md-6 col-md-offset-3 form-group">
+        <label>Quote</label>
+        <textarea class="form-control" rows="3" v-model="quote"></textarea>
+      </div>
+      <div class="col-sm-8 col-sm-offset-2 col-xs-12 col-md-6 col-md-offset-3 form-group">
+      <button class="btn btn-primary" @click.prevent="createNew">新增Quote</button> <!-- 8/25 1.v-bind createNew methods, prevent防reload -->
+      </div>
+    </form>
+
+  </div>
+</template>
+
+<script>
+export default {
+  // set up quotes
+  data () {
+    return {
+      quote: ''
+    }
+  },
+  // 8/25 2. Add New quote methods
+  methods: {
+    createNew () {
+      this.$emit('addQuote', this.quote) //Back to App.vue
+      this.quote = ''
+    }
+  }
+  }
+</script>
